0bba0c38192fa1ab304e86ce8fc0ccf4b0e1f80c
David A. Mann <davidmann10k@gmail.com>
2017-08-24 14:22:35 -0500

    adds account dropdown

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,12 +5,14 @@ class PagesController < ApplicationController
     @trade = Trade.new(
       credit: Entry.new(credit: true), 
       debit:  Entry.new(credit: false)
-    )
-    @trades = current_user.trades.includes(:debit, :credit) if current_user
-    
+    )    
   end
 
   def portfolio
     @entry_totals = current_user.entry_totals if current_user
+  end
+
+  def transactions
+    @trades = current_user.trades.includes(:debit, :credit)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: "pages#index"
   
   get '/portfolio', to: 'pages#portfolio'
+  get '/transactions', to: 'pages#transactions'
 
   devise_for :users
 
